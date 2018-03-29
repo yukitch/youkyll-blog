8dc092416cc35a41bc735695c2e7e34b5db8e6de
youkyll <yuuki08180102@gmail.com>
2018-03-30 06:13:58 +0900

    update cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,4 @@
 import CMS from 'netlify-cms';
 import 'netlify-cms/dist/cms.css';
 
-import BlogPostPreview from './preview-templates/BlogPostPreview';
-
 CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
